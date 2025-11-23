42bddb839582d4f8dcd1e83548ed7989afc8a603
dmitriy.sudakov <dmitriy.sudakov@red-soft.ru>
2025-11-23 18:57:25 +0300

    Add UserController for CRUD operations

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,89 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicateDataException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Имейл должен быть указан");
+        }
+        if (isUserExists(user)) {
+            throw new DuplicateDataException("Этот имейл уже используется");
+        }
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        if (newUser.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (users.containsKey(newUser.getId())) {
+            User oldUser = users.get(newUser.getId());
+            if (newUser.getEmail() == null || newUser.getEmail().isBlank()) {
+                throw new ConditionsNotMetException("Имейл должен быть указан");
+            }
+            // if email was changed
+            if (!newUser.getEmail().equals(oldUser.getEmail()) && isUserExists(newUser)) {
+                throw new DuplicateDataException("Этот имейл уже используется");
+            }
+            if (newUser.getPassword() != null) {
+                oldUser.setPassword(newUser.getPassword());
+            }
+            if (newUser.getEmail() != null) {
+                oldUser.setEmail(newUser.getEmail());
+            }
+            if (newUser.getUsername() != null) {
+                oldUser.setUsername(newUser.getUsername());
+            }
+            return oldUser;
+        }
+        throw new NotFoundException("Пост с id = " + newUser.getId() + " не найден");
+    }
+
+    private boolean isUserExists(User user) {
+        Optional<User> savedUser = users.values().stream()
+                .filter(iterUser -> iterUser.getEmail().equals(user.getEmail()))
+                .findFirst();
+        return savedUser.isPresent();
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
